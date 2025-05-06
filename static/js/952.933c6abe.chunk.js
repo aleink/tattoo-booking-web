bf63ff92b76bf22c313473ce587be979edd61091
Your Name <you@example.com>
2025-05-06 13:23:26 -0700

    Updates

---
+++ static/js/952.933c6abe.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktattoo_booking_web=self.webpackChunktattoo_booking_web||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.933c6abe.chunk.js.map
